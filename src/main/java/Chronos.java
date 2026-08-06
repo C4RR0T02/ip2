9f614dcf00071589a07170e80a09aee3c379524f
Sherlena Ngui <66990228+C4RR0T02@users.noreply.github.com>
2026-08-06 11:14:51 +0800

    Add Chronos welcome and goodbye messages

--- src/main/java/Chronos.java
+++ src/main/java/Chronos.java
@@ -3,12 +3,18 @@
  */
 public class Chronos {
     public static void main(String[] args) {
+        String separator = "____________________________________________________________";
         String banner = "  ██████╗██╗  ██╗██████╗  ██████╗ ███╗   ██╗ ██████╗ ███████╗\n"
                 + " ██╔════╝██║  ██║██╔══██╗██╔═══██╗████╗  ██║██╔═══██╗██╔════╝\n"
                 + " ██║     ███████║██████╔╝██║   ██║██╔██╗ ██║██║   ██║███████╗\n"
                 + " ██║     ██╔══██║██╔══██╗██║   ██║██║╚██╗██║██║   ██║╚════██║\n"
                 + " ╚██████╗██║  ██║██║  ██║╚██████╔╝██║ ╚████║╚██████╔╝███████║\n"
-                + "  ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚══════╝\n";
+                + "  ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚══════╝";
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println("Hello! I'm Chronos.\nWhat can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
